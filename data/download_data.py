67e031aec9d5f7e1a9e7a688167b03738e52eca3
Tani Barasch <Netanel.Barasch@mail.huji.ac.il>
2020-01-23 15:21:40 -0800

    .

--- data/download_data.py
+++ data/download_data.py
@@ -1,30 +1,44 @@
+# Author: Tani Barasch
+# date: 2020-23-01
 
+"""Downloads data csv from the web to a local filepath as a csv file format.
+Usage: src/down_data.py --url=<url> --out_file=<out_file>
+Options:
+--url=<url>              URL from where to download the data (must be in standard csv format)
+--out_file=<out_file>    Path (including filename) of where to locally write the file
+"""
 
+from docopt import docopt
+import requests
+import os
+import pandas as pd
 
 url1 = 'https://projects.fivethirtyeight.com/nfl-api/nfl_elo.csv'
-
 url2 = 'https://projects.fivethirtyeight.com/nfl-api/nfl_elo_latest.csv'
-historic_df = pd.read_csv(url1)
-current_df = pd.read_csv(url2)
 
-def main(url,file_name, path):
 
-# documentation comments
+opt = docopt(__doc__)
 
-# import libraries/packages
-    import pandas as pd
+def main(url, out_file):
+    
+    # test url
+    try: 
+        request = requests.get(url)
+        request.status_code == 200
+    except Exception as req:
+        print("Website at url is broken.")
+        print(req)
 
-# parse/define command line arguments here
+    # get dataframe from url 
+    data = pd.read_csv(url, )
 
-# define main function
-    path = "../data"
-    df = pd.read_csv(url)
-    df.to_csv("historic_nfl_elo.csv")
-    current_df.to_csv("current_nfl_elo.csv")
-    # code for "guts" of script goes here
-
-# code for other functions & tests goes here
+    # save df as csv
+    try:
+        data.to_csv(out_file, index = False)
+    except:
+        os.makedirs(os.path.dirname(out_file))
+        data.to_csv(out_file, index = False)
 
 # call main function
 if __name__ == "__main__":
-    main()
+    main(opt["--url"],opt["--out_file"])
