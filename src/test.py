f970e925477aba3037fc5bf672338a4b4a267e8e
Tani Barasch <Netanel.Barasch@mail.huji.ac.il>
2020-01-23 19:18:06 -0800

    complete wrang script

--- src/test.py
+++ src/test.py
@@ -1,23 +1,8 @@
-# Author: Tani Barasch
-# date: 2020-23-01
-
-"""Downloads data csv from the web to a local filepath as a csv file format.
-Usage: src/down_data.py --url=<url> --out_file=<out_file>
-Options:
---url=<url>              URL from where to download the data (must be in standard csv format)
---out_file=<out_file>    Path (including filename) of where to locally write the file
-"""
-
 from docopt import docopt
 import requests
 import os
 import pandas as pd
 
-# historic data url: https://projects.fivethirtyeight.com/nfl-api/nfl_elo.csv
-# 2019 data url:     https://projects.fivethirtyeight.com/nfl-api/nfl_elo_latest.csv
-
-
-opt = docopt(__doc__)
 
 def main(url, out_file):
 
@@ -41,6 +26,8 @@ def main(url, out_file):
 
     # save df as csv, if file path doesnt exist, create file path
     try:
+        print(os.getcwd())
+        print(out_file)
         data.to_csv(out_file, index = False)
     except:
         os.makedirs(os.path.dirname(out_file))
@@ -51,10 +38,4 @@ def main(url, out_file):
     except: 
         raise Exception("file save fail")
 
-#main('https://projects.fivethirtyeight.com/nfl-api/nfl_elo_latest.csv','../data/elo_test.csv')
-
-
-
-# call main function
-if __name__ == "__main__":
-    main(opt["--url"],opt["--out_file"])
+main(url = 'https://projects.fivethirtyeight.com/nfl-api/nfl_elo_latest.csv',out_file = '../data/test_elo.csv')
