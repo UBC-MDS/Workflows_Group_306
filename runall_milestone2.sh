0f753091cfc3b335d1012c0571455bb17949c1d4
Simardeep Kaur <simardeep.kaur.jeji@gmail.com>
2020-01-25 01:26:33 -0700

    added bash command

--- runall_milestone2.sh
+++ runall_milestone2.sh
@@ -2,3 +2,7 @@
 
 # run eda report
 Rscript -e "rmarkdown::render('src/EDA_milestone2.Rmd')"
+
+#Run random forest and logistic regression on the cleaned data
+python src/model.py --train_file=data/elo_historic_wrangled.csv --test_file=data/elo_2019_wrangled.csv
+
